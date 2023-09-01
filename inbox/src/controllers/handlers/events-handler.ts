70b90c50bb4016b8487724bcbbf90676f32262f5
Agustina Aldasoro <agusaldasoro@gmail.com>
2023-09-01 16:25:30 -0300

    feat: read notifications from db

--- inbox/src/controllers/handlers/events-handler.ts
+++ inbox/src/controllers/handlers/events-handler.ts
@@ -1,13 +1,35 @@
 import { Readable } from 'node:stream'
-import { HandlerContextWithPath } from '../../types'
+import { HandlerContextWithPath, InvalidRequestError, UsersNotification } from '../../types'
 
-export async function eventsHandler(context: HandlerContextWithPath<'logs', '/notifications/events'>) {
-  const { logs } = context.components
+import SQL from 'sql-template-strings'
+
+export async function eventsHandler(context: HandlerContextWithPath<'logs' | 'pg', '/notifications/events'>) {
+  const { pg, logs } = context.components
   const logger = logs.getLogger('Events Handler')
 
-  const interval = setInterval(() => {
-    logger.log('pushing')
-    stream.push(`data: ${JSON.stringify({ now: performance.now() })}\n\n`)
+  let from = context.url.searchParams.get('from') || 0
+  const userId = context.url.searchParams.get('userId')?.toString()
+
+  if (!userId || userId === '') {
+    throw new InvalidRequestError('Missing userId')
+  }
+
+  const interval = setInterval(async () => {
+    // Every second, send a "ping" event.
+    stream.push(`event: ping\n`)
+
+    const query = SQL`SELECT * FROM users_notifications u JOIN notifications n ON u.notification_id = n.id
+    WHERE LOWER(u.address) = LOWER(${userId})
+    AND u.timestamp > to_timestamp(${from} / 1000.0)
+    ORDER BY u.timestamp DESC`
+
+    logger.debug(`Query: ${query.text}`)
+
+    const notifications = await pg.query<UsersNotification>(query)
+    for (const notification of notifications.rows) {
+      stream.push(`data: ${JSON.stringify(notification)}\n\n`)
+    }
+    from = notifications.rows[0]?.timestamp || from
   }, 1500)
 
   const stream = new Readable({
--- inbox/src/types.ts
+++ inbox/src/types.ts
@@ -45,3 +45,44 @@ export type HandlerContextWithPath<
 >
 
 export type Context<Path extends string = any> = IHttpServerComponent.PathAwareContext<GlobalContext, Path>
+
+export type NotificationError = {
+  error: string
+  message: string
+}
+
+export class InvalidRequestError extends Error {
+  constructor(message: string) {
+    super(message)
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+export class NotFoundError extends Error {
+  constructor(message: string) {
+    super(message)
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+/// DB
+
+export type UsersNotification = {
+  id: string
+  address: string
+  notification_id: string
+  timestamp: number
+  read: boolean
+  created_at: number
+  updated_at: number
+}
+
+export type Notification = {
+  id: string
+  type: string
+  source: string
+  metadata: any
+  timestamp: number
+  created_at: number
+  updated_at: number
+}
