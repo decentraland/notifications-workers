c4f0891e124345499885ce22a04281ca8f8ece7a
Mariano Goldman <mariano.goldman@decentraland.org>
2023-11-29 16:27:16 -0300

    add pg metrics

--- inbox/src/metrics.ts
+++ inbox/src/metrics.ts
@@ -1,8 +1,10 @@
 import { getDefaultHttpMetrics, validateMetricsDeclaration } from '@well-known-components/metrics'
 import { metricDeclarations as logMetricDeclarations } from '@well-known-components/logger'
+import { metricDeclarations as pgMetricDeclarations } from '@well-known-components/pg-component'
 
 export const metricDeclarations = {
   ...getDefaultHttpMetrics(),
+  ...pgMetricDeclarations,
   ...logMetricDeclarations
 }
 
