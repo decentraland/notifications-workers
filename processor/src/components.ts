13c97511482df04c8a05b392b914e50ea93061d9
Mariano Goldman <mariano.goldman@decentraland.org>
2023-12-01 09:37:25 -0300

    wip

--- processor/src/components.ts
+++ processor/src/components.ts
@@ -33,6 +33,7 @@ export async function initComponents(): Promise<AppComponents> {
     const dbHost = await config.requireString('PG_COMPONENT_PSQL_HOST')
     const dbPassword = await config.requireString('PG_COMPONENT_PSQL_PASSWORD')
     databaseUrl = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbDatabaseName}`
+    console.log('Using database url: ' + databaseUrl)
   }
   // This worker writes to the database, so it runs the migrations
   const pg = await createPgComponent(
