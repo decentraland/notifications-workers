f339147adf7fb775ba277fceae0bbf68203f7f0b
Mariano Goldman <mariano.goldman@decentraland.org>
2023-12-04 17:38:53 -0300

    Validation for external notifications

--- processor/test/integration/publish-notification-handler.spec.ts
+++ processor/test/integration/publish-notification-handler.spec.ts
@@ -3,28 +3,25 @@ import { getIdentity, Identity } from '../utils'
 
 test('POST /notifications', function ({ components, stubComponents }) {
   let identity: Identity
-  beforeAll(async () => {
-    identity = await getIdentity()
-  })
-
   beforeEach(async () => {
+    identity = await getIdentity()
     const { config } = stubComponents
     config.requireString.withArgs('INTERNAL_API_KEY').resolves('some-api-key')
   })
 
   async function findNotification(someEventKey: string, type: string, address: string) {
-    const result = await components.pg.query(
-      `SELECT *
-         FROM notifications
-         WHERE event_key = '${someEventKey}'
-           AND type = '${type}'
-           AND address = '${address.toLowerCase()}'`
-    )
+    const result = await components.pg.query(`
+                SELECT *
+                FROM notifications
+                WHERE event_key = '${someEventKey}'
+                  AND type = '${type}'
+                  AND address = '${address.toLowerCase()}'
+        `)
     return result.rows[0]
   }
 
   it('should publish a new notification', async () => {
-    const { pg, db, localFetch } = components
+    const { localFetch } = components
 
     const notification = {
       eventKey: 'some-event-key',
@@ -33,8 +30,7 @@ test('POST /notifications', function ({ components, stubComponents }) {
       metadata: {
         test: `This is a test at ${new Date().toISOString()}`
       },
-      timestamp: Date.now(),
-      read_at: null
+      timestamp: Date.now()
     }
 
     const response = await localFetch.fetch('/notifications', {
@@ -54,8 +50,34 @@ test('POST /notifications', function ({ components, stubComponents }) {
     expect(found.timestamp).toEqual(`${notification.timestamp}`)
   })
 
+  it('should reject invalid notification body', async () => {
+    const { localFetch } = components
+
+    const notification = {
+      type: 'test',
+      eventKey: 'some-event-key',
+      address: identity.realAccount.address,
+      wrongMetadata: {
+        test: `This is a test at ${new Date().toISOString()}`
+      },
+      timestamp: Date.now()
+    }
+
+    const response = await localFetch.fetch('/notifications', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer some-api-key`
+      },
+      body: JSON.stringify(notification)
+    })
+
+    expect(response.status).toEqual(400)
+    expect(await response.json()).toMatchObject({ error: 'Bad request', message: '"metadata" is required' })
+    expect(await findNotification(notification.eventKey, notification.type, notification.address)).toBeUndefined()
+  })
+
   it('should be protected by api key', async () => {
-    const { pg, db, localFetch } = components
+    const { localFetch } = components
 
     const response = await localFetch.fetch('/notifications', {
       method: 'POST',
