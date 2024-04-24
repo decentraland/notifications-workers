cc768b1fe27b175d32b0ac0df0275096499d36a5
Mariano Goldman <mariano.goldman@decentraland.org>
2024-04-24 11:25:15 -0300

    chore: update libraries

--- processor/src/metrics.ts
+++ processor/src/metrics.ts
@@ -1,7 +1,8 @@
-import { getDefaultHttpMetrics, validateMetricsDeclaration } from '@well-known-components/metrics'
+import { validateMetricsDeclaration } from '@well-known-components/metrics'
 import { metricDeclarations as logMetricDeclarations } from '@well-known-components/logger'
 import { metricDeclarations as pgMetricDeclarations } from '@well-known-components/pg-component'
 import { metricDeclarations as theGraphMetricDeclarations } from '@well-known-components/thegraph-component'
+import { getDefaultHttpMetrics } from '@well-known-components/http-server'
 
 export const metricDeclarations = {
   ...getDefaultHttpMetrics(),
