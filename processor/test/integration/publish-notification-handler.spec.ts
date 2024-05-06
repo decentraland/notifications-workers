f9506fd078b594405c424e20cc6c8640704623bf
Mariano Goldman <marianogoldman@users.noreply.github.com>
2024-05-06 17:15:17 -0300

    feat: external notification endpoint only accepts known notification types (#60)

--- processor/test/integration/publish-notification-handler.spec.ts
+++ processor/test/integration/publish-notification-handler.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '../components'
 import { getIdentity, Identity } from '../utils'
+import { NotificationType } from '@dcl/schemas'
 
 test('POST /notifications', function ({ components }) {
   let identity: Identity
@@ -26,7 +27,7 @@ test('POST /notifications', function ({ components }) {
 
     const notification = {
       eventKey: 'some-event-key',
-      type: 'test',
+      type: NotificationType.BID_RECEIVED,
       address: identity.realAccount.address,
       metadata: {
         test: `This is a test at ${new Date().toISOString()}`
@@ -44,7 +45,7 @@ test('POST /notifications', function ({ components }) {
 
     expect(response.status).toEqual(204)
 
-    const found = await findNotification('some-event-key', 'test', identity.realAccount.address)
+    const found = await findNotification('some-event-key', NotificationType.BID_RECEIVED, identity.realAccount.address)
     expect(found).toBeDefined()
     expect(found.metadata).toEqual(notification.metadata)
     expect(found.read_at).toBeNull()
@@ -52,6 +53,32 @@ test('POST /notifications', function ({ components }) {
   })
 
   it('should reject invalid notification body', async () => {
+    const { localFetch } = components
+
+    const notification = {
+      type: NotificationType.BID_RECEIVED,
+      eventKey: 'some-event-key',
+      address: identity.realAccount.address,
+      wrongMetadata: {
+        test: `This is a test at ${new Date().toISOString()}`
+      },
+      timestamp: Date.now()
+    }
+
+    const response = await localFetch.fetch('/notifications', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      },
+      body: JSON.stringify([notification])
+    })
+
+    expect(response.status).toEqual(400)
+    expect(await response.json()).toMatchObject({ error: 'Bad request', message: '"[0].metadata" is required' })
+    expect(await findNotification(notification.eventKey, notification.type, notification.address)).toBeUndefined()
+  })
+
+  it('should reject invalid notification type', async () => {
     const { localFetch } = components
 
     const notification = {
@@ -73,7 +100,10 @@ test('POST /notifications', function ({ components }) {
     })
 
     expect(response.status).toEqual(400)
-    expect(await response.json()).toMatchObject({ error: 'Bad request', message: '"[0].metadata" is required' })
+    expect(await response.json()).toMatchObject({
+      error: 'Bad request',
+      message: `"[0].type" must be one of [${Object.values(NotificationType).join(', ')}]`
+    })
     expect(await findNotification(notification.eventKey, notification.type, notification.address)).toBeUndefined()
   })
 
