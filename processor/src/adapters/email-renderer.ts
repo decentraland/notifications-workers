ba7e41b38c72450a7b541f9b9e31c853ff185d54
Mariano Goldman <mariano.goldman@decentraland.org>
2024-05-16 21:03:08 -0300

    simplify formatting in templates

--- processor/src/adapters/email-renderer.ts
+++ processor/src/adapters/email-renderer.ts
@@ -6,55 +6,47 @@ import { Email } from '@notifications/common'
 import { AppComponents, IEmailRenderer, NotificationRecord } from '../types'
 import { formatMana } from '../logic/utils'
 
-const nonTransformer = (notification: NotificationRecord): NotificationRecord => notification
-
-const formatPriceAsMana = (notification: NotificationRecord) => ({
-  ...notification,
-  metadata: {
-    ...notification.metadata,
-    price: formatMana(notification.metadata.price)
-  }
-})
-
-const formatRoyaltiesCutAsMana = (notification: NotificationRecord) => ({
-  ...notification,
-  metadata: {
-    ...notification.metadata,
-    royaltiesCut: formatMana(notification.metadata.royaltiesCut)
-  }
-})
-
-const transformers: Partial<Record<NotificationType, (notification: NotificationRecord) => NotificationRecord>> = {
-  [NotificationType.BID_ACCEPTED]: formatPriceAsMana,
-  [NotificationType.BID_RECEIVED]: formatPriceAsMana,
-  [NotificationType.ROYALTIES_EARNED]: formatRoyaltiesCutAsMana
+enum TemplatePart {
+  SUBJECT = 'subject',
+  CONTENT = 'content'
 }
 
-function loadTemplates(part: string) {
-  return Object.values(NotificationType)
-    .map((type) => fs.readFileSync(path.join(__dirname, `email-templates/${type}.${part}.handlebars`), 'utf8'))
-    .reduce(
-      (acc, template, index) => {
-        acc[Object.values(NotificationType)[index]] = handlebars.compile(template, {})
-        return acc
-      },
-      {} as Record<NotificationType, HandlebarsTemplateDelegate>
-    )
+function loadTemplates() {
+  handlebars.registerHelper('formatMana', (mana: string) => formatMana(mana))
+
+  return Object.values(NotificationType).reduce(
+    (acc, notificationType) => {
+      acc[notificationType] = {
+        [TemplatePart.SUBJECT]: handlebars.compile(
+          fs.readFileSync(
+            path.join(__dirname, `email-templates/${notificationType}.${TemplatePart.SUBJECT}.handlebars`),
+            'utf8'
+          )
+        ),
+        [TemplatePart.CONTENT]: handlebars.compile(
+          fs.readFileSync(
+            path.join(__dirname, `email-templates/${notificationType}.${TemplatePart.CONTENT}.handlebars`),
+            'utf8'
+          )
+        )
+      }
+      return acc
+    },
+    {} as Record<NotificationType, Record<TemplatePart, HandlebarsTemplateDelegate>>
+  )
 }
 
 export async function createRenderer(components: Pick<AppComponents, 'subscriptionService'>): Promise<IEmailRenderer> {
   const { subscriptionService } = components
 
-  const contentTemplates = loadTemplates('content')
-  const subjectTemplates = loadTemplates('subject')
+  const templates = loadTemplates()
 
   async function renderEmail(notification: NotificationRecord): Promise<Email> {
     const subscription = await subscriptionService.findSubscriptionForAddress(notification.address)
-    const transformed = (transformers[notification.type] || nonTransformer)(notification)
     return {
       to: subscription?.email || '',
-      subject: subjectTemplates[notification.type](transformed),
-      content: contentTemplates[notification.type](transformed)
+      subject: templates[notification.type][TemplatePart.SUBJECT](notification),
+      content: templates[notification.type][TemplatePart.CONTENT](notification)
     }
   }
 
