ce5f638e6818ce466900078f83b0aad5462dc5c7
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-09-05 14:16:09 -0300

    fix: prevent returning badge-granted notification on dApps

--- inbox/src/controllers/handlers/notifications-handler.ts
+++ inbox/src/controllers/handlers/notifications-handler.ts
@@ -1,8 +1,14 @@
+import { NotificationType } from '@dcl/schemas'
 import { HandlerContextWithPath } from '../../types'
 import { NotificationDb } from '@notifications/common'
 
+const EXCLUDED_NOTIFICATIONS_ON_DAPPS = [NotificationType.BADGE_GRANTED]
+
 export async function notificationsHandler(
-  context: Pick<HandlerContextWithPath<'db' | 'logs', '/notifications'>, 'url' | 'components' | 'verification'>
+  context: Pick<
+    HandlerContextWithPath<'db' | 'logs', '/notifications'>,
+    'url' | 'request' | 'components' | 'verification'
+  >
 ) {
   const { db } = context.components
   const searchParams = context.url.searchParams
@@ -10,11 +16,12 @@ export async function notificationsHandler(
   const onlyUnread = searchParams.has('onlyUnread')
   const limitParam = parseInt(searchParams.get('limit') || '20', 10)
   const limit = !!limitParam && limitParam > 0 && limitParam <= 50 ? limitParam : 20
+  const isCallFromExplorer: boolean = context.request.headers.get('Origin') === 'explorer'
 
   const userId = context.verification!.auth.toLowerCase()
 
   const notifications = await db.findNotifications([userId], onlyUnread, from, limit)
-  const slimNotifications = notifications.map((notification: NotificationDb) => ({
+  let slimNotifications = notifications.map((notification: NotificationDb) => ({
     id: notification.id,
     type: notification.type,
     address: notification.address,
@@ -22,6 +29,12 @@ export async function notificationsHandler(
     timestamp: notification.timestamp,
     read: !!notification.read_at || !!notification.broadcast_read_at
   }))
+
+  if (!isCallFromExplorer) {
+    slimNotifications = slimNotifications.filter(
+      (notification) => !EXCLUDED_NOTIFICATIONS_ON_DAPPS.includes(notification.type as NotificationType)
+    )
+  }
 
   return {
     body: { notifications: slimNotifications }
